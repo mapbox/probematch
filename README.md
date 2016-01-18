1938e50164413d806555222d9265ce54ae20f607
Tim Channell <tim.channell@gmail.com>
2016-01-18 17:07:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+Match probe data to roads by distance and bearing
+
+#### install
+
+`npm install probematch`
+
+### usage
+
+*probematch* creates an [rbush index](https://github.com/mourner/rbush) of a road network and allows you to match individual probe points to the roads by a combo of distance and bearing. Both distance and bearing filters are configurable.
+
+```js
+var probematch = require('probematch');
+
+var roads = {
+  'type': 'FeatureCollection'
+  'features': [
+    // Linestring features representing the road network
+  ]
+};
+
+var match = probematch(roads, {
+  compareBearing: true, // should bearing be used to filter matches?
+  maxProbeDistance: 0.01, // distance filter in kilometers for probe matching
+  maxBearingRange: 5 // maximum bearing difference, in degrees, allowed for match filtering
+});
+
+var probe = /* a point feature representing a probe */;
+var probeBearing  = /* a bearing in degrees */
+
+var results = match(probe, probeBearing);
+```
+
+### output
+
+*probematch* outputs an array of potential matches, sorted by distance from the probe.
+
+```js
+[
+  {
+    "line": /* the road geometry that matched */,
+    "segment": /* the segment of road that matched */,
+    "distance":0.004666787054452857
+  },
+  {
+    "segment": /* ... */,
+    "line": /* ... */,
+    "distance":0.005917207788475857
+  }
+]
+```
+
+
+### performance
+
+*probematch* performs best with the smallest road network possible loaded into it's index. Ideally, it is used on a tile-per-tile basis (for example, as part of a [tile-reduce](https://github.com/mapbox/tile-reduce) job)
---
+++ probematch.js
@@ -0,0 +1,98 @@
+var rbush = require('rbush');
+var extent = require('turf-extent');
+var xtend = require('xtend');
+var flatten = require('geojson-flatten');
+var normalize = require('geojson-normalize');
+var point = require('turf-point');
+var linestring = require('turf-linestring');
+var destination = require('turf-destination');
+var pointOnLine = require('turf-point-on-line');
+var distance = require('turf-distance');
+var bearing = require('turf-bearing');
+
+module.exports = function (inputLines, opts) {
+  var options = xtend({
+    maxProbeDistance: 0.01, // max kilometers away a probe can be to be considered a match
+    rbushMaxEntries: 9,
+    compareBearing: true, // should bearing be used to filter matches?
+    maxBearingRange: 5 // max bearing degrees allowed between a probe and a potentially matching road
+  }, opts);
+
+  var tree = rbush(options.rbushMaxEntries);
+  var lines = normalize(flatten(inputLines));
+  var load = [];
+  var segments = [];
+
+  for (var i = 0; i < lines.features.length; i++) {
+    var coords = lines.features[i].geometry.coordinates;
+    for (var j = 0; j < coords.length - 1; j++) {
+      var seg = linestring([coords[j], coords[j + 1]], {
+        lineId: i,
+        segmentId: j
+      });
+      var ext = padBbox(extent(seg), options.maxProbeDistance);
+      seg.properties.bearing = bearing(point(coords[j]), point(coords[j + 1]));
+      if (seg.properties.bearing < 0) seg.properties.bearing += 360;
+
+      ext.id = segments.length;
+
+      load.push(ext);
+      segments.push(seg);
+    }
+  }
+  tree.load(load);
+
+  var match = function (pt, bearing) {
+    var ext = padBbox(extent(pt), options.maxProbeDistance);
+    var hits = tree.search(ext);
+    var matches = [];
+
+    if (options.compareBearing && bearing === null) return [];
+    if (bearing && bearing < 0) bearing = bearing + 360;
+
+    for (var i = 0; i < hits.length; i++) {
+      var segment = segments[hits[i].id];
+      var parent = lines.features[segment.properties.lineId];
+
+      if (options.compareBearing &&
+        (bearing < segment.properties.bearing - options.maxBearingRange ||
+          bearing > segment.properties.bearing + options.maxBearingRange)
+      ) {
+        continue;
+      }
+
+      var p = pointOnLine(segment, pt);
+      var dist = distance(pt, p, 'kilometers');
+
+      if (dist <= options.maxProbeDistance) {
+        matches.push({segment: segment, line: parent, distance: dist});
+      }
+    }
+
+    matches.sort(function (a, b) {
+      if (a.distance < b.distance) { return -1; }
+      if (a.distance > b.distance) { return 1; }
+      return 0;
+    });
+    return matches;
+  };
+
+  match.tree = tree;
+  match.options = options;
+
+  return match;
+};
+
+function padBbox(bbox, tolerance) {
+  var sw = point([bbox[0], bbox[1]]);
+  var ne = point([bbox[2], bbox[3]]);
+  var newSw = destination(sw, tolerance, -135, 'kilometers');
+  var newNe = destination(ne, tolerance, 45, 'kilometers');
+
+  return [
+    newSw.geometry.coordinates[0],
+    newSw.geometry.coordinates[1],
+    newNe.geometry.coordinates[0],
+    newNe.geometry.coordinates[1]
+  ];
+}
